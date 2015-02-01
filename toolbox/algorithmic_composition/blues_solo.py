fb672d04ed85edff9b6069cb04e84a4f03b729cc
Paul Ruvolo <paullundyruvolo@gmail.com>
2015-02-01 12:13:39 -0500

    checkin of algorithmic music composition toolbox

---
+++ toolbox/algorithmic_composition/blues_solo.py
@@ -0,0 +1,34 @@
+from Nsound import *
+import numpy as np
+from numpy.random import choice, rand, random_sample
+
+def add_note(out, instr, key_num, duration, bpm, volume):
+    """ Adds a note from the given instrument to the specified stream
+
+        out: the stream to add the note to
+        instr: the instrument that should play the note
+        key_num: the piano key number (A 440Hzz is 49)
+        duration: the duration of the note in beats
+        bpm: the tempo of the music
+        volume: the volume of the note
+	"""
+    freq = (2.0**(1/12.0))**(key_num-49)*440.0
+    stream = instr.play(duration*(60.0/bpm),freq)
+    stream *= volume
+    out << stream
+
+# this controls the sample rate for the sound file you will generate
+sampling_rate = 44100.0
+Wavefile.setDefaults(sampling_rate, 16)
+
+bass = GuitarBass(sampling_rate)	# use a guitar bass as the instrument
+solo = AudioStream(sampling_rate, 1)
+
+""" these are the piano key numbers for a 3 octave blues scale in A
+	See: http://en.wikipedia.org/wiki/Blues_scale """
+blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
+beats_per_minute = 45				# Let's make a slow blues solo
+
+add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
+
+solo >> "blues_solo.wav"
