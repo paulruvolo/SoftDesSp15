2abaeb03f3fb0c56f4e841ce64fdf9cbcae862c8
Benjamin Hill <ben@csl.cornell.edu>
2015-02-09 13:46:02 -0500

    Updated assignment framework
     - Moved art generation to its own function
     - Added test_image function to test PIL install
     - Added color_map encapsulation function
     - Changed PIL Image import so it also works with Pillow
     - PEP8 compliance (mostly)

--- computational_art/recursive_art.py
+++ computational_art/recursive_art.py
@@ -1,22 +1,27 @@
 """ TODO: Put your header comment here """
 
-import Image
+import random
+from PIL import Image
+
 
 def build_random_function(min_depth, max_depth):
-    """ Builds a random function of depth at least min_depth and depth at most
-        max_depth (see assignment writeup for definition of depth in this context)
+    """ Builds a random function of depth at least min_depth and depth
+        at most max_depth (see assignment writeup for definition of depth
+        in this context)
 
         min_depth: the minimum depth of the random function
         max_depth: the maximum depth of the random function
         returns: the randomly generated function represented as a nested list
                  (see assignment writeup for details on the representation of
-                 these functions) """
+                 these functions)
+    """
     # TODO: implement this
     pass
 
+
 def evaluate_random_function(f, x, y):
     """ Evaluate the random function f with inputs x,y
-        The representation of the function f is a defined in the assignment writeup
+        Representation of the function f is defined in the assignment writeup
 
         f: the function to evaluate
         x: the value of x to be used to evaluate the function
@@ -31,16 +36,22 @@ def evaluate_random_function(f, x, y):
     # TODO: implement this
     pass
 
+
 def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
-    """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
-        to the output interval [output_interval_start, output_interval_end].
-    
-        val: the value to remap 
-        input_interval_start: the start of the interval that contains all possible values for val
-        input_interval_end: the end of the interval that contains all possible values for val
-        output_interval_start: the start of the interval that contains all possible output values
-        output_inteval_end: the end of the interval that contains all possible output values
-        returns: the value remapped from the input to the outptu interval
+    """ Given an input value in the interval [input_interval_start,
+        input_interval_end], return an output value scaled to fall within
+        the output interval [output_interval_start, output_interval_end].
+
+        val: the value to remap
+        input_interval_start: the start of the interval that contains all
+                              possible values for val
+        input_interval_end: the end of the interval that contains all possible
+                            values for val
+        output_interval_start: the start of the interval that contains all
+                               possible output values
+        output_inteval_end: the end of the interval that contains all possible
+                            output values
+        returns: the value remapped from the input to the output interval
 
         >>> remap_interval(0.5, 0, 1, 0, 10)
         5.0
@@ -52,21 +63,84 @@ def remap_interval(val, input_interval_start, input_interval_end, output_interva
     # TODO: implement this
     pass
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-    im = Image.new("RGB",(350,350))
-    pixels = im.load()
 
+def color_map(val):
+    """ Maps input value between -1 and 1 to an integer 0-255, suitable for
+        use as an RGB color code.
+
+        val: value to remap, must be a float in the interval [-1, 1]
+        returns: integer in the interval [0,255]
+
+        >>> color_map(-1.0)
+        0
+        >>> color_map(1.0)
+        255
+        >>> color_map(0.0)
+        127
+        >>> color_map(0.5)
+        191
+    """
+    # NOTE: This relies on remap_interval, which you must provide
+    color_code = remap_interval(val, -1, 1, 0, 255)
+    return int(color_code)
+
+
+def test_image(filename, x_size=350, y_size=350):
+    """ Generate test image with random pixels and save as an image file.
+
+        filename: string filename for image (should be .png)
+        x_size, y_size: optional args to set image dimensions (default: 350)
+    """
+    # Create image and loop over all pixels
+    im = Image.new("RGB", (x_size, y_size))
+    pixels = im.load()
+    for i in range(x_size):
+        for j in range(y_size):
+            x = remap_interval(i, 0, x_size, -1, 1)
+            y = remap_interval(j, 0, y_size, -1, 1)
+            pixels[i, j] = (random.randint(0, 255),  # Red channel
+                            random.randint(0, 255),  # Green channel
+                            random.randint(0, 255))  # Blue channel
+
+    im.save(filename)
+
+
+def generate_art(filename, x_size=350, y_size=350):
+    """ Generate computational art and save as an image file.
+
+        filename: string filename for image (should be .png)
+        x_size, y_size: optional args to set image dimensions (default: 350)
+    """
+    # Functions for red, green, and blue channels - where the magic happens!
     red_function = ["x"]
     green_function = ["y"]
     blue_function = ["x"]
-    for i in range(im.size[0]):
-        for j in range(im.size[1]):
-            x = remap_interval(i,0,im.size[0],-1,1)
-            y = remap_interval(j,0,im.size[1],-1,1)
-            pixels[i,j] = (int(remap_interval(evaluate_random_function(red_function,x,y),-1,1,0,255)),
-                           int(remap_interval(evaluate_random_function(green_function,x,y),-1,1,0,255)),
-                           int(remap_interval(evaluate_random_function(blue_function,x,y),-1,1,0,255)))
 
-    im.save('myart.png')
+    # Create image and loop over all pixels
+    im = Image.new("RGB", (x_size, y_size))
+    pixels = im.load()
+    for i in range(x_size):
+        for j in range(y_size):
+            x = remap_interval(i, 0, x_size, -1, 1)
+            y = remap_interval(j, 0, y_size, -1, 1)
+            pixels[i, j] = (
+                    color_map(evaluate_random_function(red_function, x, y)),
+                    color_map(evaluate_random_function(green_function, x, y)),
+                    color_map(evaluate_random_function(blue_function, x, y))
+                    )
+
+    im.save(filename)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    # Create some computational art!
+    # TODO: Un-comment the generate_art function call after you
+    #       implement remap_interval and evaluate_random_function
+    #generate_art("myart.png")
+
+    # Test that PIL is installed correctly
+    # TODO: Comment or remove this function call after testing PIL install
+    test_image("noise.png")
