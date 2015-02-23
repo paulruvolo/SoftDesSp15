c5462e71cbfaa400fef048475fa9721eb242a207
Paul Ruvolo <paullundyruvolo@gmail.com>
2015-02-23 10:45:53 -0500

    ml toolbox added

---
+++ toolbox/ml/learning_curve.py
@@ -0,0 +1,26 @@
+""" Exploring learning curves for classification of handwritten digits """
+
+import matplotlib.pyplot as plt
+import numpy
+from sklearn.datasets import *
+from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+data = load_digits()
+print data.DESCR
+n_trials = 5
+train_percentages = range(5,95,5)
+test_accuracies = numpy.zeros(len(train_percentages))
+
+# train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
+# the resultant accuracy.
+# You should repeat each training percentage n_trials times to smooth out variability
+# for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
+
+# TODO: your code here
+
+fig = plt.figure()
+plt.plot(train_percentages, test_accuracies)
+plt.xlabel('Percentage of Data Used for Training')
+plt.ylabel('Accuracy on Test Set')
+plt.show()
