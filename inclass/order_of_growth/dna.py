cc9457379f300bebc04686382d7440eb9742c6dc
Paul Ruvolo <paullundyruvolo@gmail.com>
2015-03-30 11:51:49 -0400

    added a DNA reverse complement example

---
+++ inclass/order_of_growth/dna.py
@@ -0,0 +1,73 @@
+""" Evaluate the performance of various DNA proessing algorithms """
+
+from random import randint
+import time
+
+def generate_random_dna(n):
+    """ Generate a random DNA sequence of length n """
+    dna_list = []
+    nucleotides = ['A','C','G','T']
+    for i in range(n):
+        r = randint(0,3)
+        dna_list.append(nucleotides[r])
+    return "".join(dna_list)
+
+def get_complement(c):
+    """ Returns the complimentary nucleotide to c """
+    if c == 'A':
+        return 'T'
+    if c == 'C':
+        return 'G'
+    if c == 'G':
+        return 'C'
+    if c == 'T':
+        return 'A'
+
+def reverse_complement_1(dna):
+    """ Method 2 for computing the reverse complementary sequence of DNA
+        for the specfied DNA sequence
+    
+        dna: a DNA sequence represented as a string
+        returns: the reverse complementary DNA sequence represented as a string
+    >>> reverse_complement_1("ATGCCCGCTTT")
+    'AAAGCGGGCAT'
+    >>> reverse_complement_1("CCGCGTTCA")
+    'TGAACGCGG'
+    """
+    return_val = ""
+    for c in reversed(dna):
+        return_val += get_complement(c)
+    return return_val
+
+def reverse_complement_2(dna):
+    """ Method 1 for Computing the reverse complementary sequence of DNA
+        for the specfied DNA sequence
+    
+        dna: a DNA sequence represented as a string
+        returns: the reverse complementary DNA sequence represented as a string
+    >>> reverse_complement_2("ATGCCCGCTTT")
+    'AAAGCGGGCAT'
+    >>> reverse_complement_2("CCGCGTTCA")
+    'TGAACGCGG'
+    """
+    return_val = []
+    for c in reversed(dna):
+        return_val.append(get_complement(c))
+    return "".join(return_val)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    dna = generate_random_dna(5000)
+    start_time = time.time()
+    rev_complement = reverse_complement_1(dna)
+    stop_time = time.time()
+
+    print stop_time - start_time
+
+    dna = generate_random_dna(50000)
+    start_time = time.time()
+    rev_complement = reverse_complement_2(dna)
+    stop_time = time.time()
+
+    print stop_time - start_time
